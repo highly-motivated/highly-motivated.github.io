89d03a796ad9398e8a666583d08ccf16d6fe4256
highly-motivated <167588522+highly-motivated@users.noreply.github.com>
2024-04-23 10:00:46 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,110 @@
+const translations = {
+    'en': {
+        // menu
+        'nav_about': 'About',
+        'nav_pricing': 'Pricing',
+        'nav_events': 'Events',
+        'nav_membership': 'Membership',
+        'nav_register': 'Register',
+        'nav_contact': 'Contact',
+        // about us
+        'about_title': 'About Us',
+        'about_text': 'We are a community of cannabis enthusiasts dedicated to promoting safe and social use of cannabis. Join us to learn, share, and experience cannabis in a responsible and inviting environment.', 
+        'about_text_2': 'Starting July 1, 2024, a new era begins! We welcome the end of the restrictive culture and offer a wide range of products. As a Cannabis Social Club, we will have the opportunity to apply for a cultivation license from this date. Once we have received the license, we will start the long-awaited cultivation. According to legal regulations, adults over the age of 21 may obtain up to 50 grams of cannabis per month. Younger adults aged 18 to 21 may purchase up to 30 grams, but with a THC content of no more than 10%.',
+        // goals
+        'goals_title': 'Our Goals',
+        'goals_text': 'We aim to educate and engage the community on the benefits and responsible use of cannabis and to coordinating the collective cultivation and distribution of cannabis to our members according to the highest quality standards.',
+        'goal_full_legalization': 'Full legalization of cannabis',
+        'goal_world_class_quality': 'Providing world-class quality for our members',
+        'goal_cannabis_education': 'Cannabis education',
+        'goal_youth_protection': 'Youth protection',
+        'goal_safe_harbor': 'Safe Harbor',
+        // pricing
+        'pricing_title': 'Pricing',
+        'pricing_text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+        // events
+        'events_title': 'Upcoming Events',
+        'events_text': 'Our events range from educational seminars to social gatherings, all aimed at enhancing understanding and enjoyment of cannabis in a communal setting.',
+        // membership
+        'membership_title': 'Become a Member',
+        'membership_text': 'Members get exclusive access to all cannabis products, free access to our events, special discounts on products and services, and a first look at new offerings.',
+        'membership_bold': 'To become a member contact us via social media or use the contact form.',
+        'membership_text_2': 'We offer only 500 exclusive memberships. Sign up now to become part of our community. Please note that you must be of legal age and able to demonstrate residency in Germany. For the verification of your information, we require a copy of your ID. Membership is subject to a fee*. We will carefully review your application and contact you as soon as possible.',
+        'registration_fee': '€50 (einmalig)',
+        'monthly_fee': '€25',
+        'membership_text_3': '*Your monthly membership fee contributes to covering the rent, administration, projects, and content costs of the club.',
+        'membership_text_3_bold': 'Consumables and other services are not included.',
+        // registration
+        'registration_title': 'Registration',
+        'registration_text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+        // footer
+        'contact_title': 'Contact Us',
+        'location_title': 'Our Location',
+        'terms': 'Terms and Conditions',
+        'privacy': 'Privacy Policy',
+        'cookies': 'Cookie Policy',
+        'impress': 'Impress',
+        
+    },
+
+    'de': {
+        // menu
+        'nav_about': 'Über uns',
+        'nav_pricing': 'Preise',
+        'nav_events': 'Veranstaltungen',
+        'nav_membership': 'Mitgliedschaft',
+        'nav_register': 'Anmeldung',
+        'nav_contact': 'Kontakt',
+        // about us
+        'about_title': 'Über Uns',
+        'about_text': 'Wir lieben Cannabis und haben uns daher entschlossen, einen Cannabis Social Club a.k.a. eine Anbauvereinigung zu gründen. Mit dieser Anbauvereinigung werden wir eine Lizenz für den gemeinschaftlichen Anbau von Cannabis beantragen, was für alle Anbauvereinigungen ab dem 1. Juli 2024 möglich sein wird.',
+        'about_text_2': 'Je nachdem, wann die Lizenz erteilt wird, werden wir dann ab Ende des Jahres 2024 oder zu Beginn des Jahres 2025 in der Lage sein, unser erstes gemeinschaftlich angebautes Cannabis an unsere Mitglieder weiterzugeben. Wir werden uns immer bemühen, unseren Mitgliedern eine Auswahl feinster Sorten bereitzustellen. Wenn du dabei sein willst, fülle jetzt (unter „Anmeldung“) unseren Mitgliedsantrag aus! Wir prüfen deine Bewerbung und setzen uns mit dir in Verbindung.',
+        // goals
+        'goals_title': 'Unsere Ziele',
+        'goals_text': 'Wir wollen gemeinschaftlich Cannabis anbauen und an unsere Mitglieder weitergeben. Außerdem wollen wir zum Thema Cannabis informieren und uns über das Thema austauschen.',
+        'goals_text_2': 'Was uns auszeichnet:',
+        'goal_full_legalization': 'Gemeinschaftlicher Cannabisanbau und Weitergabe an unsere Mitglieder',
+        'goal_world_class_quality': 'Verschiedene Sorten Cannabis von höchster Qualität',
+        'goal_cannabis_education': 'Weitergabe von Samen und Stecklingen an unsere Mitglieder ',
+        'goal_youth_protection': 'Informationen zum Konsum und Umgang mit Cannabis',
+        'goal_safe_harbor': 'Kinder- und Jugendschutz',
+        // pricing
+        'pricing_title': 'Preise',
+        'pricing_text': 'Um Mitglied zu werden, wird eine einmalige Anmeldegebühr in Höhe von 99 EUR fällig. Außerdem musst du dich für eine monatliche Abnahmemenge entscheiden.',
+        // events
+        'events_title': 'Veranstaltungen',
+        'events_text': 'Unsere Veranstaltungen reichen von Bildungsseminaren bis zu gesellschaftlichen Zusammenkünften, die alle darauf abzielen, das Verständnis und den Genuss von Cannabis in einem gemeinschaftlichen Umfeld zu fördern.',
+        // membership
+        'membership_title': 'Werde Mitglied',
+        'membership_text': 'Unsere Anbauvereinigung wird maximal 500 Mitglieder haben. Falls du dabei sein willst, fülle jetzt unter „Anmeldung“ unseren Mitgliedsantrag aus. Bitte beachte, dass du mindestens 21 Jahre alt sein musst, um Mitglied in unserem Verein zu sein.',
+        'membership_bold': '',
+        'membership_text_2': 'Außerdem musst du in Deutschland ansässig sein. Wir benötigen eine Kopie deines Personalausweises und eine Bestätigung, dass du in keiner anderen Anbauvereinigung Mitglied bist, denn jede Person darf nur in einem Cannabis Social Club Mitglied sein. Für alle, die auf eine große Auswahl und höchste Qualität ihres Cannabis Wert legen: Werde Teil unserer Highly Motivated Community! Hierzu musst du einfach unten den Mitgliedsantrag ausfüllen. Wir werden uns bei dir melden.',
+        'registration_fee': '€50 (einmalig)',
+        'monthly_fee': '€25',
+        'membership_text_3': '*Dein monatlicher Mitgliedsbeitrag unterstützt die Deckung der Miet-, Verwaltungs-, Projekt- und Inhaltskosten des Clubs.',
+        'membership_text_3_bold': 'Nicht inbegriffen sind Konsumgüter oder weitere Dienstleistungen.',
+        // registration
+        'registration_title': 'Anmeldung',
+        'registration_text': 'Wenn du Mitglied in der Highly Motivated Anbauvereinigung werden willst, dann fülle hier unseren Mitgliedsantrag aus. Wir werden deinen Antrag prüfen und Kontakt zu dir aufnehmen.',
+        // footer
+        'contact_title': 'Kontaktiere Uns',
+        'location_title': 'Unser Standort',
+        'terms': 'AGB',
+        'privacy': 'Datenschutz',
+        'cookies': 'Cookie Richtline',
+        'impress': 'Impressum'
+    }
+};
+
+let flyers = [
+    { src: "events/flyer1.jpg", alt: "Flyer 1", id: "modal1" },
+    { src: "events/flyer2.jpg", alt: "Flyer 2", id: "modal2" },
+    { src: "events/flyer3.jpg", alt: "Flyer 3", id: "modal3" },
+    { src: "events/flyer1.jpg", alt: "Flyer 4", id: "modal4" },
+    { src: "events/flyer2.jpg", alt: "Flyer 5", id: "modal5" },
+    { src: "events/flyer3.jpg", alt: "Flyer 6", id: "modal6" },
+    { src: "events/flyer3.jpg", alt: "Flyer 7", id: "modal7" },
+    { src: "events/flyer1.jpg", alt: "Flyer 8", id: "modal8" },
+    { src: "events/flyer2.jpg", alt: "Flyer 9", id: "modal9" },
+    { src: "events/flyer3.jpg", alt: "Flyer 10", id: "modal10" },
+];
