c6d7fd08d028d0276c297122e39decb26d07c927
highly-motivated <167588522+highly-motivated@users.noreply.github.com>
2024-04-24 21:47:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const countdownDate = new Date("Jul 01, 2024 00:00:01").getTime();
+
+    const timerFunction = function() {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    };
+
+    timerFunction(); // Initial call to set the timer immediately
+    const x = setInterval(timerFunction, 1000); // Set interval to update every second
+});
